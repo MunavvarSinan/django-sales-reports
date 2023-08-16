c48fc3a12bec003849cb68282fabb99b6ef527de
MunavvarSinan <munavvarsinan8@gmail.com>
2023-08-16 09:31:26 +0530

    created base home page

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    hello = 'something'
+    return render(request, 'sales/main.html', {'h': hello}) # the dictionary is like passing a context to the template
