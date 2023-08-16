691d805f260989283be37e861cec0da905d3278d
MunavvarSinan <munavvarsinan8@gmail.com>
2023-08-16 09:47:16 +0530

    created sales list page

--- sales/urls.py
+++ sales/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view 
+from .views import ( home_view, SalesListView)
 
 app_name = 'sales'
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('list/', SalesListView.as_view(), name='list') # is we are using class based views we need to use .as_view() inorder to convert it to a view
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Sales
 # Create your views here.
 def home_view(request):
     hello = 'something'
-    return render(request, 'sales/main.html', {'h': hello}) # the dictionary is like passing a context to the template
+    return render(request, 'sales/home.html', {'h': hello}) # the dictionary is like passing a context to the template
+
+
+class SalesListView(ListView):
+    model = Sales
+    template_name = 'sales/main.html'
+    context_object_name = 'qs' # this is the default name for the query set
